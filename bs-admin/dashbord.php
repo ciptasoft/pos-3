3bfbd68e64c8e8c313f35ecc3040e188bae0ed23
thesanto <santocse@outlook.com>
2017-02-05 15:42:09 +0600

    dashbord ui added

---
+++ bs-admin/dashbord.php
@@ -0,0 +1,24 @@
+	<div class="container-fluid">
+		<div class="dashbord_menu">
+			<div class="col-lg-2">
+				<div class="dashbord_inner">
+					<ul class="list-group">
+						<li class="list-group-item"><a href="#"><b>DASHBORD</b></a></li>
+						<li class="list-group-item"><a href="#"><b>CUSTOMER</b></a></li>
+						<li class="list-group-item"><a href="#"><b>ITEMS</b></a></li>
+						<li class="list-group-item"><a href="#"><b>SUPPLIERS</b></a></li>
+						<li class="list-group-item"><a href="#"><b>REPORTS</b></a></li>
+						<li class="list-group-item"><a href="#"><b>SALES</b></a></li>
+						<li class="list-group-item"><a href="#"><b>SETTINGS</b></a></li>
+						<li class="list-group-item"><a href="#"><b>INVENTORY</b></a></li>
+						<li class="list-group-item"><a href="#"><b>BALANCE</b></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="dashbord_content">
+			<div class="col-lg-10">
+
+			</div>
+		</div>
+	</div>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+include 'bs-content/header.php';
+
+include 'bs-content/pos-body.php';
+include 'bs-admin/dashbord.php';
+
+
+include 'bs-content/footer.php';
+?>
