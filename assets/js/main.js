08c9c8a9fc3d37a5945e6a063aebff20cbe39000
thesanto <santocse@outlook.com>
2017-02-16 08:11:18 +0600

    major js update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,21 @@ var substringMatcher = function(strs) {
     return function findMatches(q, cb) {
         var matches, substringRegex;
 
-    // an array that will be populated with substring matches
-    matches = [];
+// an array that will be populated with substring matches
+matches = [];
 
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
+// regex used to determine if a string contains the substring `q`
+substrRegex = new RegExp(q, 'i');
 
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-        if (substrRegex.test(str)) {
-            matches.push(str);
-        }
-    });
+// iterate through the pool of strings and for any string that
+// contains the substring `q`, add it to the `matches` array
+$.each(strs, function(i, str) {
+    if (substrRegex.test(str)) {
+        matches.push(str);
+    }
+});
 
-    cb(matches);
+cb(matches);
 };
 };
 
@@ -38,3 +38,79 @@ $('#productName').typeahead({
     name: 'countries',
     source: substringMatcher(countries)
 });
+
+var subTotal = 0;
+var total =0;
+function addToCart(){
+    var productName = document.getElementsByName('productName')[0].value;
+    var productQuantity = document.getElementsByName('quantity')[0].value;
+    var productPrice = document.getElementsByName('price')[0].value;
+    total = productPrice*productQuantity;
+    subTotal = subTotal + total;
+    var cartTable = document.getElementById('cartTable');
+    var cartRow = cartTable.insertRow(0);
+
+    var cell1 = cartRow.insertCell(0);
+
+    var cell2 = cartRow.insertCell(1);
+    cell2.innerHTML = productName;
+
+    var cell3 = cartRow.insertCell(2);
+// var cell3i = '<a href="#" >';
+// cell3i = cell3i.concat(productQuantity);
+// var cell3j = '</a>'
+// cell3i = cell3i.concat(cell3j);
+cell3.innerHTML =  productQuantity;
+
+var cell4 = cartRow.insertCell(3);
+cell4.innerHTML = productPrice;
+
+var cell5 = cartRow.insertCell(4);
+cell5.innerHTML = total;
+var cell6 = cartRow.insertCell(5);
+$('.sub_total ul li h4').html(subTotal);
+cell6.innerHTML = '<div><a href="#" class="row_remove" ><i class="ion-close-circled" style="color:red;"></i></a></div>';
+}
+
+$('.crtTbl').on('click', '.row_remove', function(e){
+    $(this).closest('tr').remove();
+// var test = $(this).closest('tr').text();
+var myTableArray = [];
+
+$(this).closest('tr').each(function() {
+    var arrayOfThisRow = [];
+    var tableData = $(this).find('td');
+    if (tableData.length > 0) {
+        tableData.each(function() { arrayOfThisRow.push($(this).text()); });
+        myTableArray.push(arrayOfThisRow);
+    }
+});
+
+subTotal = subTotal - myTableArray[0][4];
+$('.sub_total ul li h4').html(subTotal);
+
+});
+
+$('.new_customer').slideUp();
+$(".newCustomerToggler").click(function(){
+    $('.new_customer').slideToggle();
+});
+var tempDiscVal = 0;
+function setDiscount(){
+    $('.discount ul li a').remove('a');
+    $('.discount ul li:nth-child(2)').html('<div class="input-group updateInput"><input type="number" name="updateDiscount" class="form-control" value="0" placeholder="Enter ammount"><span class="input-group-btn"><button class="btn btn-info" type="button">Update</button></span></div>');
+    tempDiscVal = document.getElementsByName('updateDiscount')[0].value;
+    var tempDisc = $('.discount ul li div');
+
+    tempDisc.addEventListener('click',function(){
+        tempDiscVal = document.getElementsByName('updateDiscount')[0].value;
+        $('.discount ul li div').remove();
+    })
+    console.log(tempDiscVal);
+    var cell3i = '<a href="#" >';
+    cell3i = cell3i.concat(tempDisc);
+    var cell3j = '</a>'
+    cell3i = cell3i.concat(cell3j);
+    // $('.discount ul li button').onclick().remove('.updateInput');
+
+}
--- bs-admin/sales.php
+++ bs-admin/sales.php
@@ -1,54 +1,104 @@
 <div class="sales_page">
 	<div class="container">
-		<div class="col-md-6 col-sm-12"> 
-			<input type="text" name="productName" id="productName" placeholder="Search for product">	
-			<input type="number" name="quantity" id="quantity" value="1">	
-			<input type="number" name="price" id="price" value="" placeholder="Price">	
-			<input type="button" class="btn btn-success" value="Add To Cart">
-		</div>
-		<div class="col-md-6 col-sm-12"> 
-			<input type="text">
-		</div>
-	</div>
-	<hr>
-	<div>
-		<div class="container">
-			<div class="item-form">
-				
-				<form action="https://demo.phppointofsale.com/index.php/sales/add" id="add_item_form" class="form-inline" autocomplete="off" method="post" accept-charset="utf-8">
-					<div class="input-group input-group-mobile contacts">
-						<span class="input-group-addon">
-							<a href="https://demo.phppointofsale.com/index.php/items/view/-1/1/sale" class="none add-new-item" title="New Item" id="new-item-mobile" tabindex="-1"><i class="icon ti-pencil-alt"></i> <span class="register-btn-text">New Item</span></a> </span>
-							<div class="input-group-addon register-mode sale-mode dropdown">
-								<a href="https://demo.phppointofsale.com/index.php/#" class="none active" tabindex="-1" title="Sale" id="select-mode-1" data-target="#" data-toggle="dropdown" aria-haspopup="true" role="button" aria-expanded="false"><i class="icon ti-shopping-cart"></i> <span class="register-btn-text">Sale</span></a> <ul class="dropdown-menu sales-dropdown">
-								<li><a tabindex="-1" href="#" data-mode="return" class="change-mode">Return</a></li>
-								<li><a tabindex="-1" href="#" data-mode="store_account_payment" class="change-mode">Store Account Payment</a></li>
-							</ul>
+		<div class="col-lg-8 col-sm-12"> 
+			<div class="search_box">
+				<div class="items">
+					<div class="col-md-6">
+						<div class="form-group">
+							<label for="productName">Type Product name</label>
+							<input type="text" name="productName" class="form-control" id="productName" placeholder="Search for product">	
 						</div>
-						<span class="input-group-addon grid-buttons ">
-							<a href="https://demo.phppointofsale.com/index.php/#" class="none show-grid" tabindex="-1" title="Show Grid"><i class="icon ti-layout"></i> <span class="register-btn-text">Show Grid</span></a> <a href="https://demo.phppointofsale.com/index.php/#" class="none hide-grid hidden" tabindex="-1" title="Hide Grid"><i class="icon ti-layout"></i> <span class="register-btn-text">Hide Grid</span></a> </span>
+					</div>
+					<div class="col-md-2">
+						<div class="form-group">
+							<label for="quantity">Set Quantity</label>
+							<input type="number" name="quantity" class="form-control" id="quantity" value="1">	
 						</div>
-						<div class="input-group contacts register-input-group">
-							
-							<div class="spinner" id="ajax-loader" style="display:none">
-								<div class="rect1"></div>
-								<div class="rect2"></div>
-								<div class="rect3"></div>
-							</div>
-							<span class="input-group-addon">
-								<a href="https://demo.phppointofsale.com/index.php/items/view/-1/1/sale" class="none add-new-item" title="New Item" id="new-item" tabindex="-1"><i class="icon ti-pencil-alt"></i></a> </span>
-								<span role="status" aria-live="polite" class="ui-helper-hidden-accessible"></span><input type="text" id="item" name="item" class="add-item-input pull-left ui-autocomplete-input" placeholder="Enter item name or scan barcode" autocomplete="off">
-								<div class="input-group-addon register-mode sale-mode dropdown">
-									<a href="https://demo.phppointofsale.com/index.php/#" class="none active" tabindex="-1" title="Sale" id="select-mode-2" data-target="#" data-toggle="dropdown" aria-haspopup="true" role="button" aria-expanded="false"><i class="icon ti-shopping-cart"></i>Sale</a> <ul class="dropdown-menu sales-dropdown">
-									<li><a tabindex="-1" href="#" data-mode="return" class="change-mode">Return</a></li>
-									<li><a tabindex="-1" href="#" data-mode="store_account_payment" class="change-mode">Store Account Payment</a></li>
-								</ul>
-							</div>
-							<span class="input-group-addon grid-buttons ">
-								<a href="https://demo.phppointofsale.com/index.php/#" class="none show-grid" tabindex="-1" title="Show Grid"><i class="icon ti-layout"></i> Show Grid</a> <a href="https://demo.phppointofsale.com/index.php/#" class="none hide-grid hidden" tabindex="-1" title="Hide Grid"><i class="icon ti-layout"></i> Hide Grid</a> </span>
-							</div>
-						</form>
+					</div>
+					<div class="col-md-2">
+						<div class="form-group">
+							<label for="price">Set Price</label>
+							<input type="number" name="price" class="form-control" id="price" value="1" placeholder="Price">	
+						</div>
+					</div>
+					<div class="col-md-2">
+						<a href="#" class="btn btn-success" onclick="addToCart()">Add to Cart</a>
 					</div>
 				</div>
 			</div>
+			<div class="bar"></div>
+			<table class="table table-hover crtTbl">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Item Name</th>
+						<th>Quantity</th>
+						<th>Price</th>
+						<th>Total</th>
+						<th>Remove</th>
+					</tr>
+				</thead>
+				<tbody  id="cartTable"></tbody>
+			</table>
 		</div>
+		<div class="col-lg-4 col-sm-12"> 
+			<div class="sales_sidebar">
+				<a href="/" class="btn btn-danger">Cancel sale</a>
+				<br>
+				<hr>
+				<div class="search_customer">
+					<label for="customerName">Enter Customer Name</label>
+					<br>
+					<div class="input-group">
+						<span class="input-group-addon" id="sizing-addon2"><a href="#" class="newCustomerToggler"><i class="ion-person-add"></i></a></span>
+						<input type="text" class="form-control" name="customerName" id="customerName" placeholder="Customer Name" aria-describedby="sizing-addon2">
+					</div>
+					<div class="new_customer">
+						<label for="Username">New Customer Information</label>
+						<div class="input-group input-group-sm">
+							<span class="input-group-addon" id="sizing-addon3"><I class="ion-person"></I></span>
+							<input type="text" class="form-control" placeholder="Username" aria-describedby="sizing-addon3">
+						</div>
+						<div class="input-group input-group-sm">
+							<span class="input-group-addon" id="sizing-addon3"><i class="ion-android-call"></i></span>
+							<input type="text" class="form-control" placeholder="Phone Number" aria-describedby="sizing-addon3">
+						</div>
+						<div class="input-group input-group-sm">
+							<span class="input-group-addon" id="sizing-addon3"><i class="ion-ios-email"></i></span>
+							<input type="text" class="form-control" placeholder="Email AAddress" aria-describedby="sizing-addon3">
+						</div>
+					</div>
+				</div>
+				<hr>
+				<div class="discount">
+					<label>Set Discount</label>
+					<ul>
+						<li>Discount On Entire Sale :</li>
+						<li style="float: right; color: red; cursor: pointer;" ><a onclick="setDiscount()" href="#"><b>Set Disc.</b></a></li>
+					</ul>
+				</div>
+				<div class="sub_total">
+					<label>Sub Total </label>
+					<ul>
+						<li>Total Ammount <br> <h4>0</h4></li>
+						<li>Ammount Due <br><h4>0</h4></li>
+					</ul>
+				</div>
+				<div class="payment_type">
+					<label>Payment Type</label>
+					<br>
+					<a href="#" class="btn btn-primary">Cash</a>
+					<a href="#" class="btn btn-warning">Check</a>
+					<div class="input-group">
+						<input type="text" class="form-control" placeholder="Search for...">
+						<span class="input-group-btn">
+							<button class="btn btn-info" type="button">Complete sale</button>
+						</span>
+					</div>
+					<label for="paymentComment">Add Comment</label>
+					<textarea name="paymentComment" id="paymentComment" rows="3"></textarea>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
